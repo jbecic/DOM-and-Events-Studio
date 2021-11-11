8afc7677cf1e728291c6a15d009599eb3341f0de
Jasmin Becic <jasminbecic@Jasmins-MacBook-Air.local>
2021-11-11 11:55:46 -0600

    trying to make window pop up

--- scripts.js
+++ scripts.js
@@ -2,9 +2,15 @@
 // Remember to pay attention to page loading!
 function init () {
     const takeOff = document.getElementById('takeOff');
+    const flightStatus = document.getElementById('flightStatus');
 
     takeOff.addEventListener('click', event => {
-        let reponse = window.confirm('Confirm that the shuttle is ready for takeoff.');
+        let response = window.confirm('Confirm that the shuttle is ready for takeoff.');
+        let str;
+        if (response === true) {
+            str = 'Shuttle in flight.';
+            flightStatus.innerHTML = str;
+        }
     })
 }
 
