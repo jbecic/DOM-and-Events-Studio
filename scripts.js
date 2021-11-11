42c46be5d412482fecb15ec589734f785d89981d
Jasmin Becic <jasminbecic@Jasmins-MacBook-Air.local>
2021-11-11 16:35:18 -0600

    worked on parts 3 and 4

--- scripts.js
+++ scripts.js
@@ -2,17 +2,36 @@
 // Remember to pay attention to page loading!
 function init() {
     const takeOff = document.getElementById('takeOff');
+    const land = document.getElementById('landing');
+    const missionAbort = document.getElementById('missionAbort');
     const flightStatus = document.getElementById('flightStatus');
     const shuttleBackground = document.getElementById('shuttleBackground');
     const spaceShuttleHeight = document.getElementById('spaceShuttleHeight');
 
-    takeOff.addEventListener('click', event => {
+    takeOff.addEventListener('click', (event) => {
         let response = window.confirm('Confirm that the shuttle is ready for takeoff.');
 
         if (response === true) {
             flightStatus.innerHTML = 'Shuttle in flight.';
             shuttleBackground.style.backgroundColor = 'blue';
             spaceShuttleHeight.innerHTML = '10,000';
+        }
+    })
+
+    land.addEventListener('click', (event) => {
+        window.alert('The shuttle is landing. Landing gear engaged.');
+        flightStatus.innerHTML = 'The shuttle has landed.';
+        shuttleBackground.style.backgroundColor = 'green';
+        spaceShuttleHeight.innerHTML = '0';
+    })
+
+    missionAbort.addEventListener('click', (event) => {
+        let response = window.confirm('Confirm that you want to abort the mission.');
+
+        if (response === true) {
+            flightStatus.innerHTML = 'Mission aborted.';
+            shuttleBackground.style.backgroundColor = 'green';
+            spaceShuttleHeight.innerHTML = '0';
         }
     })
 }
