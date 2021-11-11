b678e0d8005656a4d237593780c68bdb3e504122
Jasmin Becic <jasminbecic@Jasmins-MacBook-Air.local>
2021-11-11 11:51:32 -0600

    Added click event for takeoff

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init () {
+    const takeOff = document.getElementById('takeOff');
+
+    takeOff.addEventListener('click', event => {
+        let reponse = window.confirm('Confirm that the shuttle is ready for takeoff.');
+    })
+}
+
+
+window.addEventListener("load", init);
