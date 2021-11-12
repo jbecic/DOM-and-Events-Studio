8d01eddb9df230f6a71b262ad8d62dc5c9718ddb
Jasmin Becic <jasminbecic@Jasmins-MacBook-Air.local>
2021-11-12 11:17:04 -0600

    fixed errors

--- scripts.js
+++ scripts.js
@@ -4,16 +4,23 @@ function init() {
     const takeOff = document.getElementById('takeoff');
     const land = document.getElementById('landing');
     const missionAbort = document.getElementById('missionAbort');
-    // const up = document.getElementById('up');
-    // const down = document.getElementById('down');
-    // const right = document.getElementById('right');
-    // const left = document.getElementById('left');
-    // const rocket = document.getElementById('rocket');
+    const up = document.getElementById('up');
+    const down = document.getElementById('down');
+    const right = document.getElementById('right');
+    const left = document.getElementById('left');
+    const rocket = document.getElementById('rocket');
     const flightStatus = document.getElementById('flightStatus');
     const shuttleBackground = document.getElementById('shuttleBackground');
     const spaceShuttleHeight = document.getElementById('spaceShuttleHeight');
-    // let top = 0;
-    // let left = 0;
+    let vertical = 0;
+    let horizontal = 0;
+
+    function originalPosition() {
+        rocket.style.bottom = '0px';
+        rocket.style.left = '0px';
+        vertical = 0;
+        horizontal = 0;
+    }
 
     takeOff.addEventListener('click', () => {
         let response = window.confirm('Confirm that the shuttle is ready for takeoff.');
@@ -30,6 +37,7 @@ function init() {
         flightStatus.innerHTML = 'The shuttle has landed.';
         shuttleBackground.style.backgroundColor = 'green';
         spaceShuttleHeight.innerHTML = 0;
+        originalPosition();
     });
 
     missionAbort.addEventListener('click', () => {
@@ -39,36 +47,37 @@ function init() {
             flightStatus.innerHTML = 'Mission aborted.';
             shuttleBackground.style.backgroundColor = 'green';
             spaceShuttleHeight.innerHTML = 0;
+            originalPosition();
         }
     });
 
-    // up.addEventListener('click', () => {
-    //     top += 10;
-    //     rocket.style.bottom = `${top}px`;
-    //     if (spaceShuttleHeight.innerHTML === 0) {
-    //         spaceShuttleHeight.innerHTML = 10000;
-    //     } else {
-    //         spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
-    //     }
-    // });
+    up.addEventListener('click', () => {
+        vertical += 10;
+        rocket.style.bottom = `${vertical}px`;
+        if (spaceShuttleHeight.innerHTML === 0) {
+            spaceShuttleHeight.innerHTML = 10000;
+        } else {
+            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
+        }
+    });
 
-    // down.addEventListener('click', () => {
-    //     top -= 10;
-    //     rocket.style.bottom = `${top}px`;
-    //     if (spaceShuttleHeight.innerHTML > 0) {
-    //         spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) - 10000;
-    //     } 
-    // });
+    down.addEventListener('click', () => {
+        if (spaceShuttleHeight.innerHTML > 0) {
+            vertical -= 10;
+            rocket.style.bottom = `${vertical}px`;
+            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) - 10000;
+        } 
+    });
 
-    // right.addEventListener('click', () => {
-    //     left += 10;
-    //     rocket.style.left = `${left}px`;
-    // });
+    right.addEventListener('click', () => {
+        horizontal += 10;
+        rocket.style.left = `${horizontal}px`;
+    });
 
-    // left.addEventListener('click', () => {
-    //     left -= 10;
-    //     rocket.style.left = `${left}px`;
-    // });
+    left.addEventListener('click', () => {
+        horizontal -= 10;
+        rocket.style.left = `${horizontal}px`;
+    });
 }
 
 
