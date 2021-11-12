99b1aab712ea6db254517ff52995c083ff0c33cb
Jasmin Becic <jasminbecic@Jasmins-MacBook-Air.local>
2021-11-11 19:13:33 -0600

    added if else statements

--- scripts.js
+++ scripts.js
@@ -42,12 +42,18 @@ function init() {
 
     up.addEventListener('click', () => {
         rocket.style.top = '10px';
-        spaceShuttleHeight.innerHTML += 10000;
+        if (spaceShuttleHeight.innerHTML === 0) {
+            spaceShuttleHeight.innerHTML = 10000;
+        } else {
+            spaceShuttleHeight.innerHTML += 10000;
+        }
     });
 
     down.addEventListener('click', () => {
         rocket.style.bottom = '10px';
-        spaceShuttleHeight.innerHTML -= 10000;
+        if (spaceShuttleHeight.innerHTML > 0) {
+            spaceShuttleHeight.innerHTML -= 10000;
+        } 
     });
 
     right.addEventListener('click', () => {
